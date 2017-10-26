91802a210198c66fe4e1b48289c58f7b60353b0e
Jeff Bender <j25bender@gmail.com>
2017-10-26 13:00:01 -0600

    node tests

--- tests/Node-tests.js
+++ tests/Node-tests.js
@@ -1,10 +1,41 @@
+import { Node } from '../lib/Node.js';
 import { expect } from 'chai';
-import Node from '../lib/Node.js';
-import Trie from '../lib/Trie.js';
+import chai from 'chai';
+chai.should();
 
-describe('First Node test', () => {
-  // it('should be a object', () => {
-  // 	let newNode = new Node();
-  //   expect(newNode).to.be.an('object');
-  // })
+describe('Node Tests', () => {
+	let node;
+
+  beforeEach(() => {
+    node = new Node();
+  });
+
+  it('Expect Node to be a function', () => {
+    expect(Node).to.be.a('function');
+  })
+
+  it('Should have a property letter', () => {
+    node.should.have.property('letter');
+  })
+
+  it('Expect Node to have a letter when passed in', () => {
+  	node.letter = 'b';
+    expect(node.letter).to.equal('b');
+  })
+
+  it('Should have a property children', () => {
+    node.should.have.property('children');
+  })
+
+  it('Should start with an empty object assigned to children', () => {
+    node.children.should.be.an('object')
+  })
+
+  it('Should have a property wordEnd', () => {
+    node.should.have.property('wordEnd');
+  })
+
+  it('Should have a property popularity', () => {
+    node.should.have.property('popularity').equal(0);
+  })
 })
